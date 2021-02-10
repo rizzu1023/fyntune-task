1bf5a713882f3db4adf1169ba728ab9868005324
rizzu1023 <mrizwan9022@gmail.com>
2021-02-10 18:12:34 +0530

    blog model & controller added

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+
+Route::get('/', function () {return view('welcome');});
+
+
+Route::group(['prefix' => 'admin','middleware' => ['auth']],function(){
+    Route::resource('/blogs',\App\Http\Controllers\BlogController::class);
+});
+
+
